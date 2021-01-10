52263828a70d9e74bf882d77dd411db138b76677
n-guitar <nguitar001@gmail.com>
2021-01-10 20:47:37 +0900

    feat: function view crud sample

--- confug/urls.py
+++ confug/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include # include追加
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('samplecrud/', include('samplecrud.urls')), # 追加
+    path('samplecrud/', include('samplecrud.urls')),
 ]
