dc226a2a45c8734029f5a9f7f4d7750d73153113
nguitar001@gmail.com <nguitar001@gmail.com>
2021-02-02 20:05:57 +0900

    add_file:signup 機能追加

--- samplecrud/views.py
+++ samplecrud/views.py
@@ -2,24 +2,25 @@ from django.shortcuts import render
 from .models import Todo
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class ListPage(generic.ListView):
     model = Todo
     template_name = 'list.html'
 
-class CreatePage(generic.CreateView):
+class CreatePage(LoginRequiredMixin, generic.CreateView):
     model = Todo
     template_name = 'create.html'
     fields = ('title','memo')
     success_url = reverse_lazy('list')
 
-class UpdatePage(generic.UpdateView):
+class UpdatePage(LoginRequiredMixin, generic.UpdateView):
     model = Todo
     template_name = 'update.html'
     fields = ('title','memo')
     success_url = reverse_lazy('list')
 
-class DeletePage(generic.DeleteView):
+class DeletePage(LoginRequiredMixin, generic.DeleteView):
     model = Todo
     template_name = 'delete.html'
     success_url = reverse_lazy('list')
